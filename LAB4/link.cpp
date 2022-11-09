e5ce1b230d8ba52564f0630f0a85b1ecfde86281
GLAScorpion <pier.colella122@gmail.com>
2022-11-09 22:43:38 +0100

    link.h

--- LAB4/link.cpp
+++ LAB4/link.cpp
@@ -1,2 +1,90 @@
 #include <iostream>
 #include "link.h"
+//HELPER FUNCTIONS
+link* pop_back(link* lst){
+    return lst->back()->extract();
+}
+link* pop_front(link* lst){
+    return lst->front()->extract();
+}
+link* push_back(link* lst, link* elem){
+    return lst->back()->insert(elem);
+}
+link* push_front(link* lst, link* elem){
+    return lst->front()->add(elem);
+}
+std::string erase(link* elem){
+    elem->extract();
+    std::string tmp_val = elem->get_value();
+    delete elem;
+    return tmp_val;
+}
+//MEMBER FUNCTIONS
+link* link::find(std::string val){
+    link* elem = this;
+    link* old_elem = elem->next;
+    do{
+        if(elem->value == val) return elem;
+        elem->prev;
+    }while(elem);
+    while(old_elem){
+        if(old_elem->value == val) return elem;
+        old_elem->next;
+    }
+    return nullptr;
+}
+link* link::insert(link* new_elem) { // this_prev -- new_elem -- this
+    if (!new_elem) return this;
+    if (!this) return new_elem;
+    new_elem->next = this;
+    if (this->prev) this->prev->next = new_elem;
+    new_elem->prev = this->prev;
+    this->prev = new_elem;
+    return new_elem;
+}
+link* link::add(link* new_elem) { // this -- new_elem -- this_nxt
+    if (!new_elem) return this;
+    if (!this) return new_elem;
+    new_elem->prev = this;
+    if (this->next) this->next->prev = new_elem;
+    new_elem->next = this->next;
+    this->next = new_elem;
+    return new_elem;
+}
+link* link::back(){ //trova il primo elemento, next == nullptr
+    link* elem = this; 
+    while(elem->next){
+        elem = elem->next;
+    }
+    return elem;
+} 
+link* link::front(){    //trova l'ultimo elemento, prev == nullptr
+    link* elem = this;
+    while(elem->prev){
+        elem = elem->prev;
+    }
+    return elem;
+}
+link* link::extract(){
+    if(this->prev) this->prev->next = this->next;
+    if(this->next) this->next->prev = this->prev;
+    this->prev = nullptr;
+    this->next = nullptr;
+    return this;
+}
+
+link* link::advance(int n){
+    link* elem = this;
+    if(!elem) return nullptr;
+    while(n > 0){
+        if(!elem->next)return nullptr;
+        elem = elem->next;
+        n--;
+    }
+    while(n < 0){
+        if(!elem->prev) return nullptr;
+        elem = elem->prev;
+        n++;
+    }
+    return elem;
+}
--- LAB4/link.h
+++ LAB4/link.h
@@ -1,6 +1,29 @@
+#include <iostream>
+#include <string.h>
 class link{
-public:
-private:
-    Link* prev;
-    Link* next;
-}
+    public:
+        //COSTRUTTORI
+        link(const std::string& val, link* previous = nullptr, link* successive =nullptr)
+        : value{val}, prev{previous}, next{successive} {}
+        ~link(){ delete prev; delete next; }
+        //MEMBER FUNCTIONS
+        link* insert(link* new_elem);
+        link* add(link* new_elem);
+        link* back(); //trova il primo elemento, next == nullptr
+        link* front(); //trova l'ultimo elemento, prev == nullptr
+        link* find(std::string val);
+        link* advance(int n);
+        link* extract();     //"erase" come visto a lezione, nessuna gestione della memoria
+        link* find(std::string val);
+        std::string get_value(){ return value; }
+    private:
+        std::string value;
+        link* prev;
+        link* next;
+};
+//HELPER FUNCTIONS
+std::string erase(link* elem); //ho reinterpretato erase come un'eliminazione con distruzione, ritorna solo il valore del link
+link* pop_back(link* lst);
+link* pop_front(link* lst);
+link* push_back(link* lst, link* elem);
+link* push_front(link* lst, link* elem);
