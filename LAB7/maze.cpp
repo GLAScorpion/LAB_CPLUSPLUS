9b28da5e45bcf2f3c039cd4a7054ecb7a65296d1
GLAScorpion <pier.colella122@gmail.com>
2022-12-08 12:34:50 +0100

    Update maze.cpp

--- LAB7/maze.cpp
+++ LAB7/maze.cpp
@@ -2,33 +2,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+/*
 Maze::Maze(std::string file)
 {
     std::ifstream open_file(file);
     std::string line;
-    std::string txt;
-    std::getline(open_file, txt, '\0');
-    std::cout<<line<<std::endl;
+    std::vector<Cell> tmp;
     int x = 0;
     int y = 0;
-    bool loop = true;
-    while(loop){
-        if(open_file.eof()) loop = false;
+    while(!open_file.eof()){
         std::getline(open_file,line,'\n');
-        std::vector<Cell> tmp;
-        std::cout<<static_cast<int>(line.size())<<std::endl;
+        tmp = std::vector<Cell>();
         for(x = 0; x < static_cast<int>(line.size()-1); x++){
             tmp.push_back(Cell(line[x]));
             if(line[x] == 'S'){
                 rob_x_ = x;
                 rob_y_ = y;
             }
-        } 
-        map_.push_back(tmp);
+        }
+        if(tmp.size()>0) map_.push_back(tmp);
         y++;
     }
+    std::getline(open_file,line,'\0');
+    tmp = std::vector<Cell>();
+    for(x = 0; x < static_cast<int>(line.size()-1); x++){
+        tmp.push_back(Cell(line[x]));
+        if(line[x] == 'S'){
+            rob_x_ = x;
+            rob_y_ = y;
+        }
+    }
+    if(tmp.size()>0) map_.push_back(tmp);
     open_file.close();
-    dim_ = y-1;
+    dim_ = map_.size();
+}
+*/
+Maze::Maze(std::string file){
+    std::ifstream open_file(file);
+    std::string txt;
+    std::getline(open_file,txt,'\0');
+    open_file.close();
+    std::vector<Cell> tmp;
+    int x = 0;
+    int y = 0;
+    for(int count = 0; count < txt.size(); count++){
+        tmp = std::vector<Cell>();
+        for(x = 0; isprint(txt[x + count]) and count < txt.size(); x++){
+            tmp.push_back(Cell(txt[x + count]));
+            if(txt[count] == 'S'){
+                rob_x_ = x;
+                rob_y_ = y;
+            }
+        }
+        y++;
+        count += x;
+        if(tmp.size()>0) map_.push_back(tmp);
+    }
+    dim_ = map_.size();
 }
 Maze::Cell::Cell(const char& val){
     if(val == 'E'){
@@ -58,7 +88,7 @@ std::ostream& operator<<(std::ostream& os, std::vector<Maze::Cell> vec){
     return os;
 }
 std::ostream& operator<<(std::ostream& os, Maze val){
-    for(int i = 0; i < val.dim_; i++){
+    for(int i = 0; i < val.dim_ ; i++){
         os<<val.map_[i]<<std::endl;
     }
     return os;
